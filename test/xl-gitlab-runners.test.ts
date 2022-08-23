bc4c6ac2da24b38667f1679f7e61daf651c3761d
UHUGHLU <lucas.hughes@pearson.com>
2022-08-23 13:59:51 -0400

    Initial commit

---
+++ test/xl-gitlab-runners.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as XlGitlabRunners from '../lib/xl-gitlab-runners-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/xl-gitlab-runners-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new XlGitlabRunners.XlGitlabRunnersStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
