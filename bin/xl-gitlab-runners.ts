ecef650b3cbb37c0eed32a585eb51609c9a76ee4
UHUGHLU <lucas.hughes@pearson.com>
2022-08-23 15:10:45 -0400

    x

--- bin/xl-gitlab-runners.ts
+++ bin/xl-gitlab-runners.ts
@@ -1,21 +1,16 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { XlGitlabRunnersStack } from '../lib/xl-gitlab-runners-stack';
+import { XLGitlabRunnersStack } from '../lib/xl-gitlab-runners-stack';
 
 const app = new cdk.App();
-new XlGitlabRunnersStack(app, 'XlGitlabRunnersStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+new XLGitlabRunnersStack(app, 'XlGitlabRunnersStack', {
+  environment: "DEV",
+  token: "GR1348941AmqH_BjyPJNQYuN8FhrT",
+  env: {
+    account: "177807608173",
+    region: "us-east-1",
+  },
+  appName: "gitlab-runner",
 });
--- lib/xl-gitlab-runners-stack.ts
+++ lib/xl-gitlab-runners-stack.ts
@@ -1,16 +1,51 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import {
+  GitLabRunner,
+  PearsonStack,
+  PearsonStackProps,
+  PearsonVpc,
+} from "@pearson-cdk/constructs";
+import { InstanceClass, InstanceSize } from "aws-cdk-lib/aws-ec2";
 
-export class XlGitlabRunnersStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+import { Construct } from "constructs";
+import { StringParameter } from "aws-cdk-lib/aws-ssm";
+import { Tags } from "aws-cdk-lib";
+
+interface XLGitlabRunnersStackProps extends PearsonStackProps {
+  token: string;
+  appName: string;
+}
+
+export class XLGitlabRunnersStack extends PearsonStack {
+  constructor(scope: Construct, id: string, props: XLGitlabRunnersStackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Gitlab Runner for XL Infra repository
+    const vpc = new PearsonVpc(this, `XL-runners-vpc-${props.appName}`, {
+      vpcName: `XL-${props.appName}-vpc`,
+      cidr: "172.16.0.0/16",
+      isNaasVPC: false,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'XlGitlabRunnersQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const runner = GitLabRunner.CreateGitLabRunner(
+      this,
+      `XL-${props.appName}-runner`,
+      {
+        asgMin: 1,
+        asgMax: 3,
+        ec2class: InstanceClass.T3,
+        ec2size: InstanceSize.LARGE,
+        gitlabRunnerTokenSsmId: new StringParameter(
+          this,
+          `XL-${props.appName}-runner-parameter`,
+          {
+            stringValue: props.token,
+          }
+        ).parameterName,
+        vpc: vpc,
+        gitlabRunnerTags: [`XL-${props.appName}-${props.environment}`],
+      }
+    );
+    Tags.of(this).add("t_AppID", "SVC00753");
+    Tags.of(this).add("t_dcl", "3");
   }
 }
